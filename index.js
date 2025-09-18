d475820c5479f8f7d1d6dad75a08ca3903bfd159
furtwanglerr <140227918+furtwanglerr@users.noreply.github.com>
2025-09-18 20:55:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ client.on('messageCreate', async (message) => {
   }
 });
 
+const express = require("express");
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}
 
 
 client.login(process.env.TOKEN);
+
